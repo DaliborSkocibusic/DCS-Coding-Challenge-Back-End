6011a505abf593c54c5e87bb684890f18d319e25
Dalibor Skocibusic <skody001@mymail.unisa.edu.au>
2023-02-10 20:54:56 +1030

    First commit spring boot initialized

---
+++ Spring-Backend/src/test/java/BackendApplication/SpringBackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package BackendApplication.SpringBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
